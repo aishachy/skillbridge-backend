ecb8b2f723aaa6e665cc815cf63eff20a04b1293
Aysha Aziz <aishaazizz30@gmail.com>
2026-02-11 18:43:14 +0400

    typescript error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,5 @@
 // src/lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"; // Prisma 7+ client
+import "dotenv/config";
+
 export const prisma = new PrismaClient();
