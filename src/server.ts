186b715df995d5e30ab34169f25fbe98764144ef
Aysha Aziz <aishaazizz30@gmail.com>
2026-02-12 18:38:10 +0400

    server.ts issue

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,10 @@
-import app from "./app.js";
 import dotenv from "dotenv";
 dotenv.config();
-import { prisma } from "./lib/prisma.js";
+
+import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
-export async function main() {
-    try {
-        await prisma.$connect();
-        console.log("connected to the database successfully");
-
-        app.listen(PORT, () => {
-            console.log(`Server is running on http://localhost:${PORT}`);
-        })
-    } catch (error) {
-        console.log("an error occured", error);
-        await prisma.$disconnect();
-        process.exit(1);
-    }
-}
-
-main()
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
