4b4e4374fc2e63a887f8dcc995f7c619f8af2f25
Aysha Aziz <aishaazizz30@gmail.com>
2026-02-13 10:55:38 +0400

    deployment issue fixed

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,14 @@
-// src/lib/prisma.ts
+import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg'; 
 
-export const prisma = new PrismaClient();
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL not set in .env");
+}
+
+
+export const prisma = new PrismaClient({
+  adapter: new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+  }),
+});
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
-// src/server.ts
-import 'dotenv/config'; // Load environment variables first
-import app from './app.js'; // make sure app.js is compiled to dist
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import 'dotenv/config';
+import app from './app';
+import { prisma } from './lib/prisma'; 
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,7 +8,6 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-// Optional: graceful shutdown
 process.on('SIGINT', async () => {
   console.log('SIGINT received: closing Prisma connection');
   await prisma.$disconnect();
